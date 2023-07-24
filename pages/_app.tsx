80691e8a63739eca2935eb4e99e170566df7cd98
knh6269 <knh6269@gmail.com>
2023-07-24 17:15:41 +0900

    feat: add themeProvider (#10) themeProvider를 _app.tsx에 배치 후 theme.tsx 주입

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-import theme from '@/styles/theme'
+import themes from '@/styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={themes}>
       <Component {...pageProps} />
     </ThemeProvider>
   )
