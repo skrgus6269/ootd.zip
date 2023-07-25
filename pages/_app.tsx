376da7b39f16352b0dec45c92ddbe2ed7f426cea
knh6269 <knh6269@gmail.com>
2023-07-25 20:35:10 +0900

    setting: add globalstyle (#14) 글로벌 스타일 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '@/styles/globalStyle'
 import themes from '@/styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={themes}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={themes}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
