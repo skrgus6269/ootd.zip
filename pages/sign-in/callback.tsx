912925fd51197c4a78115f63151ad82700ad0e5c
nerdchanii <nerdchanii@gmail.com>
2023-07-25 21:49:19 +0900

    feat: add pages for sign in, sign up (#9)
    - sign out의 경우 URL이 아닌 버튼을 통해 동작하도록 변경

---
+++ pages/sign-in/callback.tsx
@@ -0,0 +1,24 @@
+import { Router, useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+export default function SignUpCallbackPage() {
+  const [count, setCount] = useState(3)
+  const router = useRouter()
+
+  useEffect(() => {
+    ;(async () => {
+      await delay(3)
+      router.push('/')
+    })()
+  }, [router])
+
+  return <div>로그인 콜백</div>
+}
+
+const delay = async (n: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(true)
+    }, n * 1000)
+  })
+}
