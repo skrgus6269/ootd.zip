9541d6c73081f7d8e24ffde8552d0a15ce6b91ee
nerdchanii <nerdchanii@gmail.com>
2023-08-01 20:13:18 +0900

    feat: add color styles(#25)

---
+++ styles/colors/priorities.ts
@@ -0,0 +1,11 @@
+const Priorities = {
+  primary: '#030303',
+  secondary: '#ffffff',
+  disabled: '#D3D3D3',
+  warn: {
+    priamry: '#ff0000',
+    secondary: '#EE0000',
+  },
+} as const;
+
+export default Priorities;
--- styles/theme.ts
+++ styles/theme.ts
@@ -2,13 +2,14 @@ import lineHeight from './font/lineHeight';
 import spacing from './font/letterSpacing';
 import weight from './font/weight';
 import fontSize from './font/fontSize';
-
+import colors from './colors';
 const theme = {
   fontSize,
   lineHeight,
   spacing,
   weight,
-};
+  colors,
+} as const;
 
 export type ThemeType = typeof theme;
 export default theme;
