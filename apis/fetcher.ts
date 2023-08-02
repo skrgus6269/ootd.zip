aa3fbd6f10e2623b28c826fd49351ca295f8a7a5
knh6269 <knh6269@gmail.com>
2023-08-02 15:50:26 +0900

    feat: modify fetcher config  type (#15)

--- apis/fetcher.ts
+++ apis/fetcher.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import { NEXT_PUBLIC_HOST } from '@/constants/develop.constants';
 import { getCookie } from '@/utils/Cookie';
 
@@ -7,22 +7,16 @@ const fetcher = axios.create({
   timeout: 2500,
 });
 
-fetcher.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+fetcher.interceptors.request.use((config) => {
   const accessToken = getCookie('accessToken');
 
   // 로그인, 회원가입 단계 api에서는 accessToken이 없으니 바로 return
   if (accessToken === undefined) return config;
 
   // 그 외의 api 에서는 accessToken을 사용해 사용자 인증
-  const newConfig = {
-    ...config,
-    headers: {
-      ...config.headers,
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
+  config.headers.Authorization = `Bearer ${accessToken}`;
 
-  return newConfig;
+  return config;
 });
 
 export default fetcher;
