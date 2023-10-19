7ea42151a325de6e62d37d0403380fb7b8da180e
knh6269 <knh6269@gmail.com>
2023-10-19 19:54:25 +0900

    feat: add TrueFalse page Input Component (#37) 진실/거짓 Input 으로 이루어진 페이지에서 사용될 컴포넌트 개발 - WeightOpen - GenderInput

---
+++ components/SignUp/GenderInput/index.tsx
@@ -0,0 +1,29 @@
+import Input from '@/components/Input';
+import { Dispatch, SetStateAction } from 'react';
+import S from './style';
+
+interface GenderInputProps {
+  gender: string;
+  setGender: Dispatch<SetStateAction<string>>;
+}
+
+export default function GenderInput({ gender, setGender }: GenderInputProps) {
+  return (
+    <S.Layout>
+      <Input>
+        <Input.Label size="big">성별</Input.Label>
+        <div className="truefalse">
+          <Input.TrueFalse
+            left="남성"
+            right="여성"
+            state={gender}
+            setState={setGender}
+          />
+        </div>
+        <Input.HelperText state={1}>
+          입력하신 정보는 스타일 추천 목적으로 활용됩니다.
+        </Input.HelperText>
+      </Input>
+    </S.Layout>
+  );
+}
