4c54073c4f1dcb6f9477128bf1bcf770644600ef
knh6269 <knh6269@gmail.com>
2023-10-19 19:52:08 +0900

    feat: add Input page Component (#37) 페이지에서 사용될 Text Input 컴포넌트들 개발 - IdInput - AgeInput - BodyInput

--- hooks/regex.ts
+++ hooks/regex.ts
@@ -1,9 +1,13 @@
 // 한글 초성 단독 사용 불가
 export const hasKoreanInitial = (value: string) => /[ㄱ-ㅎ]/g.test(value);
 
-// 특수문자 사용 불가
+// 특수문자 사용 단독사용 불가
 export const hasSpecialCharacter = (value: string) =>
-  /[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]/g.test(value);
+  /[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]/g.test(value) &&
+  value.match(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]/g)!.length === value.length;
 
-// 글자수가 12자 보다 많다면 true
+// 글자수가 12자 보다 적어야한다
 export const isMoreThan12Length = (value: string) => value.length > 12;
+
+// 글자수가 2자보다 많아야한다
+export const isMoreThan2Length = (value: string) => value.length < 2;
