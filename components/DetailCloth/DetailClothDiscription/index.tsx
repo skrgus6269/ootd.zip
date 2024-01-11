26941b3124fd5d982a12977e3af0516637f54952
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-01-11 22:10:17 +0900

    feat: add detailclothDiscription (#96) 옷 상세 설명 컴포넌트

---
+++ components/DetailCloth/DetailClothDiscription/index.tsx
@@ -0,0 +1,47 @@
+import S from './style';
+import { Headline1, Body2, Body3 } from '@/components/UI';
+import {
+  AiOutlineShopping,
+  AiOutlineCalendar,
+  AiOutlineContainer,
+  AiOutlineLink,
+} from 'react-icons/ai';
+
+interface ClothDiscriptionProps {
+  isLink: Boolean;
+  purchasing: string;
+  uploadDate?: string;
+  memo?: string;
+}
+
+export default function DetailClothDiscription({
+  isLink,
+  purchasing,
+  uploadDate,
+  memo,
+}: ClothDiscriptionProps) {
+  return (
+    <S.Layout>
+      <S.Category>
+        <S.IconSpan>
+          {isLink ? <AiOutlineLink /> : <AiOutlineShopping />}
+        </S.IconSpan>
+        <Body3>{purchasing}</Body3>
+      </S.Category>
+
+      <S.Category>
+        <S.IconSpan>
+          <AiOutlineCalendar />
+        </S.IconSpan>
+        <Body3>{uploadDate}</Body3>
+      </S.Category>
+
+      <S.Category>
+        <S.IconSpan>
+          <AiOutlineContainer />
+        </S.IconSpan>
+        <Body3>{memo}</Body3>
+      </S.Category>
+    </S.Layout>
+  );
+}
