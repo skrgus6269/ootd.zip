48a15a13c02031d875bc71cbcdd998fae2f2f627
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-01-13 14:27:02 +0900

    feat: add Header Header component style 추가

---
+++ components/Header/style.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  display: flex;
+  width: 390px;
+  height: 64px;
+  padding: 21px 20px;
+  align-items: center;
+  gap: 8px;
+  flex-shrink: 0;
+`;
+
+const S = {
+  Layout,
+};
+
+export default S;
