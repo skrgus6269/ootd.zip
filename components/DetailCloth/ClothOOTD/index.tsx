7534f76ffd9c0c0079f6ba77759c9877c4160906
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-01-17 00:00:52 +0900

    feat: update detailClothOOTD (#96) 이미지 레이아웃 추가

--- components/DetailCloth/ClothOOTD/index.tsx
+++ components/DetailCloth/ClothOOTD/index.tsx
@@ -2,20 +2,19 @@ import S from './style';
 import { Headline1, Body2, Body3 } from '@/components/UI';
 import Header from '@/components/Header';
 import SubHead from '../SubHead';
-import Gallery from '@/components/Gallery/';
+import ImageLayout from '@/components/ImageLayout';
 
 interface ClothOOTDProps {
   count: number;
+  imagePaths: string[];
 }
 
-export default function ClothOOTD({ count }: ClothOOTDProps) {
+export default function ClothOOTD({ count, imagePaths }: ClothOOTDProps) {
   return (
     <S.Layout>
       <Header text="이 옷을 활용한 OOTD" />
       <SubHead count={count} clicked="new" />
-      {/* <Gallery>
-
-        </Gallery> */}
+      <ImageLayout imagePaths={imagePaths} />
     </S.Layout>
   );
 }
