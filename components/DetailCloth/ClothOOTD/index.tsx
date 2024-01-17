1959e02563970250bf52e2a9be7b9596600eace4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-01-17 20:01:59 +0900

    refactor: update detailClothOOTD (#96) ImageList 컴포넌트로 변경

--- components/DetailCloth/ClothOOTD/index.tsx
+++ components/DetailCloth/ClothOOTD/index.tsx
@@ -2,19 +2,26 @@ import S from './style';
 import { Headline1, Body2, Body3 } from '@/components/UI';
 import Header from '@/components/Header';
 import SubHead from '../SubHead';
-import ImageLayout from '@/components/ImageLayout';
+import ImageList from '@/components/ImageList';
+
+interface OOTDDataProps {
+  imageId: number;
+  image: string;
+}
 
 interface ClothOOTDProps {
   count: number;
-  imagePaths: string[];
+  data: OOTDDataProps[];
 }
 
-export default function ClothOOTD({ count, imagePaths }: ClothOOTDProps) {
+export default function ClothOOTD({ count, data }: ClothOOTDProps) {
   return (
     <S.Layout>
       <Header text="이 옷을 활용한 OOTD" />
       <SubHead count={count} clicked="new" />
-      <ImageLayout imagePaths={imagePaths} />
+      <S.OOTDLayout>
+        <ImageList data={data} type="column" />
+      </S.OOTDLayout>
     </S.Layout>
   );
 }
