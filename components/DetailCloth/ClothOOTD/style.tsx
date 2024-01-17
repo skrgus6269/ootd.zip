18fee3d38e6e411399570899752f3f6b34e56c49
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-01-17 22:41:33 +0900

    feat: add detailClothOOTD (#96) style 추가

---
+++ components/DetailCloth/ClothOOTD/style.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const OOTDLayout = styled.div`
+  padding: 0 20px 30px 20px;
+`;
+
+const S = {
+  Layout,
+  OOTDLayout,
+};
+
+export default S;
