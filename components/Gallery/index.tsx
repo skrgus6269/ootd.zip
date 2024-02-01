4f7490ab475235c8059b8e42eaa5602f3e382699
knh6269 <knh6269@gmail.com>
2024-02-01 16:05:35 +0900

    feat: add selectPhoto page (#113)

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -3,10 +3,13 @@ import {
   getReactNativeMessage,
   sendReactNativeMessage,
 } from '@/utils/reactNativeMessage';
-import { Dispatch, SetStateAction, useEffect } from 'react';
-import { ImageWithTag } from '../AddItem/TagModal';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import Carousel from '../Carousel';
 import { useRouter } from 'next/router';
+import { ImageWithTag } from '../Domain/AddOOTD/TagModal';
+import S from './style';
+import NextButton from '../NextButton';
+import { Body3, Body4 } from '../UI';
 
 interface GalleryProps {
   setImageAndTag: Dispatch<SetStateAction<ImageWithTag | undefined | string>>;
@@ -25,6 +28,9 @@ const Gallery = ({
 }: GalleryProps) => {
   const router = useRouter();
 
+  const [images, setImages] = useState<ImageWithTag | undefined | string>([]);
+  const [realTouch, setRealTouch] = useState<number>(100);
+
   useEffect(() => {
     if (!window.ReactNativeWebView) {
       return;
@@ -34,7 +40,7 @@ const Gallery = ({
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      getReactNativeMessage(setImageAndTag);
+      getReactNativeMessage(setImages);
     }
   }, []);
 
@@ -48,30 +54,111 @@ const Gallery = ({
     }
   }, [imageAndTag]);
 
+  const [selectedImage, setSelectedImage] = useState<ImageWithTag>([]);
+
+  const onClickImage = (ootdId: number, ootdImage: string) => {
+    if (ootdId !== realTouch) {
+      setRealTouch(ootdId);
+
+      const alive = selectedImage.filter(
+        (item) => item.ootdId === ootdId
+      ).length;
+
+      if (!alive) {
+        setRealTouch(ootdId);
+        setSelectedImage([
+          ...selectedImage,
+          { ootdImage: ootdImage, ootdId: ootdId },
+        ]);
+      }
+      return;
+    }
+
+    const newSelectedImage = selectedImage.filter((item, index) => {
+      if (item.ootdId !== ootdId) {
+        return item;
+      }
+    });
+    if (selectedImage.length === 1) {
+      setRealTouch(100);
+    } else {
+      setRealTouch(newSelectedImage[newSelectedImage.length - 1].ootdId);
+    }
+
+    setSelectedImage(newSelectedImage);
+  };
+
+  const onClickNextButton = () => {};
+
   return (
-    <Carousel infinite={false} slidesToShow={1}>
-      {typeof imageAndTag !== 'string' ? (
-        imageAndTag &&
-        imageAndTag.map((item, index) => {
-          return (
-            <img
-              onClick={() => handleStep(nextStep)}
-              style={{ width: '101px' }}
-              key={index}
-              src={item.ootdImage}
-              alt=""
-            />
-          );
+    <S.Layout>
+      <S.Image>
+        {selectedImage.length === 0 && (
+          <img
+            className="bigImage"
+            src="https://ootdzip.s3.ap-northeast-2.amazonaws.com/d2ff5b49-cbe3-40b3-8aa6-62551b5f3917_2024-01-31.png"
+            alt="basic"
+          />
+        )}
+      </S.Image>
+      {typeof images !== 'string' ? (
+        selectedImage &&
+        selectedImage.map((item, index) => {
+          if (item.ootdId === realTouch)
+            return (
+              <S.Image key={index}>
+                <img className="bigImage" src={item.ootdImage} alt="" />
+              </S.Image>
+            );
         })
       ) : (
-        <img
-          onClick={() => handleStep(nextStep)}
-          style={{ width: '101px' }}
-          src={imageAndTag}
-          alt=""
-        />
+        <img onClick={() => handleStep(nextStep)} src={images} alt="" />
       )}
-    </Carousel>
+      <S.ImageList imageListlength={images!.length}>
+        <Body4 className="selected" state="emphasis">
+          {selectedImage.length}장의 사진이 선택됨
+        </Body4>
+        <Carousel
+          infinite={false}
+          slidesToShow={images!.length <= 3 ? images!.length : 3.2}
+        >
+          {typeof images !== 'string' ? (
+            images &&
+            images.map((item, index) => {
+              return (
+                <S.Image key={index} state={item.ootdId === realTouch}>
+                  <img
+                    className="smallImage"
+                    onClick={() => onClickImage(item.ootdId, item.ootdImage)}
+                    src={item.ootdImage}
+                    alt=""
+                  />
+                  {selectedImage.map((items, indexs) => {
+                    return (
+                      item.ootdId === items.ootdId && (
+                        <S.ImageNumber state={true} key={indexs}>
+                          {indexs + 1}
+                        </S.ImageNumber>
+                      )
+                    );
+                  })}
+                </S.Image>
+              );
+            })
+          ) : (
+            <img onClick={() => handleStep(nextStep)} src={images} alt="" />
+          )}
+        </Carousel>
+      </S.ImageList>
+      <Body3 className="helperText">사진을 눌러 순서를 변경해보세요.</Body3>
+      <NextButton
+        className="nextButton"
+        state={selectedImage.length > 0}
+        onClick={onClickNextButton}
+      >
+        다음
+      </NextButton>
+    </S.Layout>
   );
 };
 
--- components/Gallery/style.tsx
+++ components/Gallery/style.tsx
@@ -1,7 +1,77 @@
 import styled from 'styled-components';
 
-const Layout = styled.div``;
+const Layout = styled.div`
+  .nextButton {
+    padding: 16px 20px 24px 16px;
+  }
+  .helperText {
+    padding: 42px 0 18px 0;
+    text-align: center;
+    color: #aaaaaa;
+  }
+  .selectedImage {
+  }
+`;
 
-const S = { Layout };
+interface ImageProps {
+  state?: Boolean;
+}
+
+const Image = styled.div<ImageProps>`
+  position: relative;
+  .smallImage {
+    width: 106px;
+    height: 106px;
+    object-fit: cover;
+    padding: 0 4px;
+    border-radius: 2px;
+    opacity: ${(props) => (props.state ? 0.5 : 1)};
+  }
+  .bigImage {
+    width: 100%;
+    height: 100vw;
+    object-fit: cover;
+    border-bottom: 8px solid ${(props) => props.theme.color.grey_95};
+  }
+`;
+
+interface ImageListProps {
+  imageListlength: number;
+}
+
+const ImageList = styled.div<ImageListProps>`
+  margin: 0 16px;
+  ${(props) =>
+    props.imageListlength <= 3 &&
+    `
+    width: calc(106px * ${props.imageListlength} + 8px);
+  `}
+  .selected {
+    margin: 24px 4px 8px 4px;
+    width: 100px;
+  }
+`;
+
+interface ImageNumberProps {
+  state: Boolean;
+}
+
+const ImageNumber = styled.div<ImageNumberProps>`
+  position: absolute;
+  top: 4px;
+  right: 12px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 1px solid white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+  color: white;
+  background-color: ${(props) => (props.state ? '#0085ff' : '#9A9A9A')};
+`;
+
+const S = { Layout, Image, ImageList, ImageNumber };
 
 export default S;
