4731eeef7678baeb62e2a9d2a507cbe7e09995ad
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-02-04 14:52:30 +0900

    feat: add editMyInfo (#114) 내 정보 수정

---
+++ components/Domain/MyPage/Edit/EditMyInfo/style.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  width: 100%;
+  display: inline-flex;
+  padding: 0px 20px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 24px;
+`;
+
+const Category = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+`;
+
+const BodyInfo = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+`;
+
+const OpenStatus = styled.div`
+  display: inline-flex;
+  padding: 0px 20px;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 16px;
+  gap: 4px;
+  width: 100%;
+`;
+
+const Wrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const S = { Layout, Category, BodyInfo, OpenStatus, Wrap };
+
+export default S;
