6c6aa41f6632c8e29387273c4a44224ed2789e56
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-02-16 01:57:17 +0900

    feat: add imageCheckBoxList (#112) 이미지 체크 박스 토글 리스트

---
+++ components/ImageCheckBoxList/index.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
+import CheckBox from '../Input/CheckBox';
+import S from './style';
+import CheckBoxTrue from 'public/images/checkBoxTrue.png';
+import CheckBoxFalse from 'public/images/checkBoxFalse.png';
+import { useState } from 'react';
+
+interface ImageCheckBoxListProps {
+  data: {
+    clothId: number;
+    clothImage: string;
+  }[];
+  checkBox: Boolean;
+}
+
+export default function ImageCheckBoxList({
+  data,
+  checkBox,
+}: ImageCheckBoxListProps) {
+  const [checkedItems, setCheckedItems] = useState<number[]>([]);
+
+  const toggleChecked = (clothId: number) => {
+    if (checkedItems.includes(clothId)) {
+      setCheckedItems(checkedItems.filter((id) => id !== clothId));
+    } else {
+      setCheckedItems([...checkedItems, clothId]);
+    }
+  };
+
+  return (
+    <S.Layout>
+      {data.map((item, index) => {
+        if (item.clothId !== undefined) {
+          const isChecked = checkedItems.includes(item.clothId);
+
+          return (
+            <S.CheckBoxLayout key={index}>
+              <img src={item.clothImage} alt="" className="clothImage" />
+              {checkBox && (
+                <Image
+                  src={isChecked ? CheckBoxTrue : CheckBoxFalse}
+                  alt={`CheckBox ${isChecked ? 'True' : 'False'}`}
+                  className="checkBoxImage"
+                  onClick={() => toggleChecked(item.clothId)}
+                />
+              )}
+            </S.CheckBoxLayout>
+          );
+        }
+      })}
+    </S.Layout>
+  );
+}
