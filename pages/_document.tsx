64492580f1f15533aee8ccd68507e9437a7eaf5d
skrgus6269 <knh6269@gmail.com>
2024-03-05 00:17:09 +0900

    fix: resolve mixed content (#162)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <meta
-        name="viewport"
-        content="upgrade-insecure-requests"
-        http-equiv="Content-Security-Policy"
-      />
+      <Head>
+        <meta
+          name="viewport"
+          content="upgrade-insecure-requests"
+          http-equiv="Content-Security-Policy"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
