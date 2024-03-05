cc3750a6d89c7d5678f0d7a713a5d778420c71a3
skrgus6269 <knh6269@gmail.com>
2024-03-05 15:06:20 +0900

    fix: modify ActionSheet delete color (#165) 삭제 버튼 색상 수정

--- components/ActionSheet/style.tsx
+++ components/ActionSheet/style.tsx
@@ -13,7 +13,11 @@ const Layout = styled.div`
   border-top: 2px solid black;
 `;
 
-const ButtonWrap = styled.div`
+interface ButtonWrapType {
+  name: string;
+}
+
+const ButtonWrap = styled.div<ButtonWrapType>`
   display: flex;
   padding: 20px 0px;
   margin-left: 16px;
@@ -23,6 +27,7 @@ const ButtonWrap = styled.div`
   gap: 8px;
   flex: 1 0 0;
   border-bottom: 1px solid ${(props) => props.theme.color.grey_95};
+  color: ${(props) => props.name === '삭제' && `${props.theme.color.error}`};
 `;
 
 const S = {
