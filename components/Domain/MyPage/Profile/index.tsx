b9209b2b80099d1f427c443b8648bbafc06ddea4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-03-14 02:50:47 +0900

    feat: update mypage (#174) 마이페이지 프로필 관련 API 업데이트

--- components/Domain/MyPage/Profile/index.tsx
+++ components/Domain/MyPage/Profile/index.tsx
@@ -2,59 +2,53 @@ import { OtherProfile } from '@/components/Profile';
 import S from './style';
 import Button from '@/components/Button';
 import { Body3, Body4, Button3 } from '@/components/UI';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
-interface UserProfileDataType {
+export interface UserProfileDataType {
   userImage: string;
   userName: string;
-  follower: number;
-  following: number;
+  followerCount: number;
+  followingCount: number;
   height: number;
   weight: number;
   followingState: Boolean;
-  introduce: string;
+  description: string;
   OOTDNumber: number;
   clothNumber: number;
 }
 
-export default function Profile() {
+interface profileProps {
+  data: UserProfileDataType;
+  localUserId: number;
+  showingId: number;
+}
+
+export default function Profile({
+  data,
+  localUserId,
+  showingId,
+}: profileProps) {
   const onClickFollowButton = () => {};
   const router = useRouter();
-
-  // const [getMypageProfileData] = useCloset();
-
-  const [userProfileData, setUserProfileData] = useState<UserProfileDataType>({
-    userImage:
-      'https://image.msscdn.net/images/style/list/l_3_2023080717404200000013917.jpg',
-    userName: '낙낙',
-    follower: 72,
-    following: 132,
-    height: 177,
-    weight: 72,
-    followingState: false,
-    introduce: '간계밥',
-    OOTDNumber: 0,
-    clothNumber: 0,
-  });
 
   return (
     <S.Layout>
       <OtherProfile
         className="profile"
-        userImage={userProfileData.userImage}
-        userName={userProfileData.userName}
+        userImage={data.userImage}
+        userName={data.userName}
         isUser={true}
-        follow={userProfileData.follower}
-        myCloth={userProfileData.following}
+        follow={data.followerCount}
+        myCloth={data.followingCount}
       />
       <S.BodyInformation>
-        <Body4>{userProfileData.height}cm</Body4>
+        <Body4>{data.height}cm</Body4>
         <p className="dot">•</p>
-        <Body4>{userProfileData.weight}kg</Body4>
+        <Body4>{data.weight}kg</Body4>
       </S.BodyInformation>
       <S.Introduce>
-        <Body3>{userProfileData.introduce}</Body3>
+        <Body3>{data.description}</Body3>
       </S.Introduce>
       <Button
         className="followButton"
@@ -64,7 +58,13 @@ export default function Profile() {
         border={false}
         onClick={onClickFollowButton}
       >
-        <Button3 onClick={() => router.push(`/EditMypage`)}>팔로우</Button3>
+        {localUserId === showingId ? (
+          <Button3 onClick={() => router.push(`/EditMypage`)}>
+            프로필 수정
+          </Button3>
+        ) : (
+          <Button3 onClick={() => ''}>팔로우</Button3>
+        )}
       </Button>
     </S.Layout>
   );
--- pages/mypage/[...mypageNumber].tsx
+++ pages/mypage/[...mypageNumber].tsx
@@ -6,9 +6,40 @@ import Profile from '@/components/Domain/MyPage/Profile';
 import Closet from '@/components/Domain/MyPage/Closet';
 import { useState, useEffect } from 'react';
 import Toast from '@/components/Toast';
+import { userId } from '@/utils/recoil/atom';
+import { useRecoilValue } from 'recoil';
+import { UserApi } from '@/apis/domain/User/UserApi';
+import { UserProfileDataType } from '@/components/Domain/MyPage/Profile';
 
 export default function MyPage() {
   const router = useRouter();
+  const path = router.asPath;
+  const [showingId, setShowingId] = useState<number>(3);
+
+  const [userProfileData, setUserProfileData] = useState<UserProfileDataType>({
+    userImage:
+      'https://image.msscdn.net/images/style/list/l_3_2023080717404200000013917.jpg',
+    userName: '낙낙',
+    followerCount: 72,
+    followingCount: 132,
+    height: 177,
+    weight: 72,
+    followingState: false,
+    description: '간계밥',
+    OOTDNumber: 0,
+    clothNumber: 0,
+  });
+
+  const { getMypage } = UserApi();
+
+  useEffect(() => {
+    const ferchData = async () => {
+      const result = await getMypage(showingId);
+      setUserProfileData(result);
+    };
+
+    ferchData();
+  });
 
   useEffect(() => {
     if (router.query.state !== '') {
@@ -17,6 +48,7 @@ export default function MyPage() {
   }, []);
 
   const [queryState, setQueryState] = useState<string>('');
+  const localUserId = useRecoilValue(userId);
 
   return (
     <>
@@ -36,8 +68,12 @@ export default function MyPage() {
             />
           }
         />
-        <Profile />
-        <Closet />
+        <Profile
+          data={userProfileData}
+          localUserId={localUserId}
+          showingId={showingId}
+        />
+        <Closet localUserId={localUserId} showingId={showingId} />
         {queryState === 'editSuccess' && (
           <Toast text="프로필이 수정되었습니다." />
         )}
