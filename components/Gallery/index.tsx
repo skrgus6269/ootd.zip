c13aa50550281ecea1731b5b8ecd5e71bae1591c
skrgus6269 <knh6269@gmail.com>
2024-03-25 00:16:16 +0900

    fix: add onClick with number (#219) 선택 사진 숫자 클릭 시 onClick 메소드 실행

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -162,14 +162,27 @@ const Gallery = ({
                       if (item.ootdId === items.ootdId) flag = 0;
                       return (
                         item.ootdId === items.ootdId && (
-                          <S.ImageNumber state={true} key={indexs}>
+                          <S.ImageNumber
+                            onClick={() =>
+                              onClickImage(item.ootdId, item.ootdImage)
+                            }
+                            state={true}
+                            key={indexs}
+                          >
                             <Caption1>{indexs + 1}</Caption1>
                           </S.ImageNumber>
                         )
                       );
                     })}
                     {flag === 1 && (
-                      <S.ImageNumber state={false}>{''}</S.ImageNumber>
+                      <S.ImageNumber
+                        onClick={() =>
+                          onClickImage(item.ootdId, item.ootdImage)
+                        }
+                        state={false}
+                      >
+                        {''}
+                      </S.ImageNumber>
                     )}
                   </S.Image>
                 );
