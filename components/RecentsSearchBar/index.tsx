87dc3399fb92697ff0ae86b08542e2ed60d5c2b8
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-03-27 23:54:29 +0900

    feat: update recentsSearchBar (#233) 엔터키를 누른 이후에만 검색 API 작동

--- components/RecentsSearchBar/index.tsx
+++ components/RecentsSearchBar/index.tsx
@@ -25,10 +25,11 @@ export default function SearchBar({
   searchValue,
 }: SearchProps) {
   const router = useRouter();
+  const [value, setValue] = useState<string>('');
 
   const onChangeSearch = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchValue(e.target.value);
+      setValue(e.target.value);
     },
     []
   );
@@ -36,24 +37,26 @@ export default function SearchBar({
   const onSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      if (searchValue.trim() !== '') {
+      if (value.trim() !== '') {
         // 검색어가 빈 문자열이 아닌 경우에만 실행
-        onAddKeyword(searchValue);
+        onAddKeyword(value);
         setState(true);
+        setSearchValue(value);
 
         // URL에 검색어 추가
         router.push({
           pathname: '/search', // 검색 결과 페이지 URL
-          query: { q: searchValue }, // 'q'라는 쿼리 매개변수에 검색어 추가
+          query: { q: value }, // 'q'라는 쿼리 매개변수에 검색어 추가
         });
       }
     },
-    [searchValue, router, onAddKeyword, setState]
+    [value, router, onAddKeyword, setState]
   );
 
   // delete 아이콘 클릭 시 실행
   const onClickCloseIcon = () => {
     setSearchValue('');
+    setValue('');
     // URL에서 검색어 제거
     router.push({
       pathname: '/search', // 검색 결과 페이지 URL
@@ -68,11 +71,7 @@ export default function SearchBar({
             <AiOutlineSearch />
           </SearchIcon>
           <SearchInput>
-            <Input
-              value={searchValue}
-              onChange={onChangeSearch}
-              placeholder="검색"
-            />
+            <Input value={value} onChange={onChangeSearch} placeholder="검색" />
           </SearchInput>
           {searchValue && (
             <CloseIcon>
