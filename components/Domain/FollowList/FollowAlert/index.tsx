accd0fc71df7f587322f3f8a0939539783691b2c
skrgus6269 <knh6269@gmail.com>
2024-04-09 13:24:20 +0900

    fix: modify alert content (#264) 차단 alert 컨텐츠 수정

--- components/Domain/FollowList/FollowAlert/index.tsx
+++ components/Domain/FollowList/FollowAlert/index.tsx
@@ -1,27 +1,30 @@
 import Alert from '@/components/Alert';
-import { Body3, Title1 } from '@/components/UI';
+import { Body3 } from '@/components/UI';
 
 interface FollowAlertProps {
   onClickYesButton: () => void;
   onClickNoButton: () => void;
+  userName: string;
 }
 
 export default function FollowAlert({
   onClickNoButton,
   onClickYesButton,
+  userName,
 }: FollowAlertProps) {
   return (
     <Alert
       onClickYesButton={onClickYesButton}
       onClickNoButton={onClickNoButton}
-      headline="팔로워에서 삭제하시겠습니까?"
+      headline={`${userName}님을 차단하시겠습니까?`}
       body={
         <>
-          <Body3>Username님은 회원님의 팔로워 리스트에</Body3>
-          <Body3>서 삭제된 것을 알 수 없습니다.</Body3>
+          <Body3>차단을 누를 경우 낙현바보님의 프로필 및</Body3>
+          <Body3>게시물(댓글)이 표시되지 않습니다. 낙현바</Body3>
+          <Body3>보님은 차단된 사실을 알 수 없습니다.</Body3>
         </>
       }
-      yes={'삭제'}
+      yes={'차단'}
       yesColor="error"
       no={'닫기'}
     />
