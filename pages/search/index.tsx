820b4484d03a7586efee2fb16b5479b0ef3efbb1
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-04-11 21:02:32 +0900

    feat: update recentsSeachBar (#279) 최근 검색어 누른 경우 Input에 유지

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -70,6 +70,7 @@ export default function Search() {
         <SearchResult keywordsValue={searchValue} />
       ) : (
         <Recents
+          setSearchValue={setSearchValue}
           handleClearKeywords={() => setKeywords([])}
           keywords={keywords}
           handleRemoveKeyword={handleRemoveKeyword}
