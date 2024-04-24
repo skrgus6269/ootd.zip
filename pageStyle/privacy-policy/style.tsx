f7eed192203f2279c5873fa2e2a702b434af53f0
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2024-04-24 22:27:35 +0900

    feat: add privacy-policy (#328) 개인정보 처리방침 UI

---
+++ pageStyle/privacy-policy/style.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
+const Text = styled.div`
+  padding: 20px;
+`;
+
+const S = { Layout, Text };
+
+export default S;
