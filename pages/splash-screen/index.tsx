9d103d84c49e1697fc7c0e8c7a420f8a0b80aef8
skrgus6269 <knh6269@gmail.com>
2024-05-08 17:02:29 +0900

    feat: add userInfoComplete Api to splash-screen (#381) 스플래시 스크린에도 유저 정보 입력 완료 유무 판단 api 추가

--- pages/splash-screen/index.tsx
+++ pages/splash-screen/index.tsx
@@ -8,20 +8,26 @@ import { PublicApi } from '@/apis/domain/Public/PublicApi';
 import { useSetRecoilState } from 'recoil';
 import { userId } from '@/utils/recoil/atom';
 import { getReactNativeMessage } from '@/utils/reactNativeMessage';
+import { RegisterApi } from '@/apis/domain/Register/RegisterApi';
 
 const SplashScreen: ComponentWithLayout = () => {
   const router = useRouter();
   const { getUserId } = PublicApi();
   const setUserId = useSetRecoilState(userId);
   const [_state, setState] = useState();
+  const { getCheckCompleteRegistUserInfo } = RegisterApi();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       getReactNativeMessage(setState);
       if (localStorage.getItem('accessToken')) {
         const result = await getUserId();
-        router.push('/main');
-        setUserId(result);
+        if (await getCheckCompleteRegistUserInfo()) {
+          router.push('/main');
+          setUserId(result);
+          return;
+        }
+        router.replace('/sign-up');
         return;
       }
       router.push('/onboarding');
